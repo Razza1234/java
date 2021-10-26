cad11b35ee0d03b93c3dc69232cd26ef28163e4f
Razza1234 <84871781+Razza1234@users.noreply.github.com>
2021-10-26 07:34:22 +0500

    Add files via upload

---
+++ java/app.js
@@ -0,0 +1,26 @@
+var User_Name= prompt("Enter your User Name")
+var School= prompt("Enter your School Name")
+var Standard= prompt("Enter your Standard")
+var Roll_no= prompt("Enter your Roll no")
+var Math= prompt("Enter your Math Mark")
+var English= prompt("Enter your English Mark")
+var Chemistry= prompt("Enter your Chemistry Mark")
+var Physics= prompt("Enter your Physics Mark")
+var Computer= prompt("Enter your Computer Mark")
+var Urdu= prompt("Enter your Urdu Mark")
+
+document.write (User_Name +"<br>")
+document.write (School +"<br>")
+document.write (Standard +"<br>")
+document.write (Roll_no +"<br>")
+document.write (Math+"<br>")
+document.write (English +"<br>")
+document.write (Chemistry +"<br>")
+document.write (Physics +"<br>")
+document.write (Computer +"<br>")
+document.write (Urdu +"<br>")
+
+
+
+
+
